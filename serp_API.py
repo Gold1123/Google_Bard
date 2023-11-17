d2e7507b2bc62e172d5df04ead825bc39bd6098f
Iaroslav Kovalchuk <iaroslavkovalchuk@gmail.com>
2023-11-17 14:29:51 +0000

    g

--- serp_API.py
+++ serp_API.py
@@ -21,22 +21,20 @@ results = search.get_dict()
 place_result = results.get('place_results')
 if not place_result:
     first_place = results.get('local_results')[0]
-    # print(first_place)
-    if 'gps_coordinates' in first_place:
-        lat = first_place['gps_coordinates']['latitude']
-        lng = first_place['gps_coordinates']['longitude']
-        data_id = first_place['data_id']
-        params = {
-            "engine": "google_maps",
-            "type": "place",
-            "data": f"!4m5!3m4!1s{data_id}!8m2!3d{lat}!4d{lng}",
-            "api_key": os.getenv("SERP_API_KEY")
-        }
-        search = GoogleSearch(params)
-        results = search.get_dict()
-        print(results['search_metadata']['google_maps_url'])
-        # return results['search_metadata']['google_maps_url']
-    else:
-        print("No GPS coordinates found for this place.")
+# print(first_place)
+if 'gps_coordinates' in first_place:
+    lat = first_place['gps_coordinates']['latitude']
+    lng = first_place['gps_coordinates']['longitude']
+    data_id = first_place['data_id']
+    params = {
+        "engine": "google_maps",
+        "type": "place",
+        "data": f"!4m5!3m4!1s{data_id}!8m2!3d{lat}!4d{lng}",
+        "api_key": os.getenv("SERP_API_KEY")
+    }
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    print(results['search_metadata']['google_maps_url'])
+    # return results['search_metadata']['google_maps_url']
 else:
-    print("No place results found.")
+    print("No GPS coordinates found for this place.")
