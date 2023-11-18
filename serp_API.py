de52cb3f47bb5918d7ad26963ec7954d13d8e8f6
Iaroslav Kovalchuk <iaroslavkovalchuk@gmail.com>
2023-11-18 13:28:05 +0000

    place

--- serp_API.py
+++ serp_API.py
@@ -7,34 +7,28 @@ load_dotenv()
 params = {
     "engine": "google_maps",
     "q": "Granite Restaurant",
-    "type": "search",
+    "type": "place",
     "api_key": os.getenv("SERP_API_KEY")
 }
-
 search = GoogleSearch(params)
 results = search.get_dict()
-
-# print(os.getenv("SERP_API_KEY"))
-search = GoogleSearch(params)
-results = search.get_dict()
-# print(results)
-first_place = results.get('place_results')
-if not first_place:
-    first_place = results.get('local_results')[0]
-# print(first_place)
-if 'gps_coordinates' in first_place:
-    lat = first_place['gps_coordinates']['latitude']
-    lng = first_place['gps_coordinates']['longitude']
-    data_id = first_place['data_id']
-    params = {
-        "engine": "google_maps",
-        "type": "place",
-        "data": f"!4m5!3m4!1s{data_id}!8m2!3d{lat}!4d{lng}",
-        "api_key": os.getenv("SERP_API_KEY")
-    }
-    search = GoogleSearch(params)
-    results = search.get_dict()
-    print(results['search_metadata']['google_maps_url'])
-    # return results['search_metadata']['google_maps_url']
-else:
-    print("No GPS coordinates found for this place.")
+print(results)
+# first_place = results.get('place_results')
+# if not first_place:
+#     first_place = results.get('local_results')[0]
+# if 'gps_coordinates' in first_place:
+#     lat = first_place['gps_coordinates']['latitude']
+#     lng = first_place['gps_coordinates']['longitude']
+#     data_id = first_place['data_id']
+#     params = {
+#         "engine": "google_maps",
+#         "type": "place",
+#         "data": f"!4m5!3m4!1s{data_id}!8m2!3d{lat}!4d{lng}",
+#         "api_key": os.getenv("SERP_API_KEY")
+#     }
+#     search = GoogleSearch(params)
+#     results = search.get_dict()
+#     print(results['search_metadata']['google_maps_url'])
+#     # return results['search_metadata']['google_maps_url']
+# else:
+#     print("No GPS coordinates found for this place.")
